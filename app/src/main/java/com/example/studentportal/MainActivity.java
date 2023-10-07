c2008c88a55f1a998c28ac29ba1cc63b609bf036
AdityaKothari27 <adityakothari2003@gmail.com>
2023-10-07 16:16:46 +0530

    Second commit

--- app/src/main/java/com/example/studentportal/MainActivity.java
+++ app/src/main/java/com/example/studentportal/MainActivity.java
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textview1);
+        textView.setText("Hello World");
 
     }
+
 }
